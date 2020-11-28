214f24b4ae73871c935090c0fc26322d7f8892d7
morgancollado <Morgan.collado@gmail.com>
2020-11-27 21:09:55 -0600

    commit tree c0c98f953b35181f2f281c82beb6294762ff3cd4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(sourceArray, callback){
+    let newArray = []
+    for (let i = 0; i < sourceArray.length; i++){
+        newArray.push(callback(sourceArray[i]))
+    }
+    return newArray
+}
+
+function reduce(sourceArray, callback, init = 0) {
+    let agg = (!!init) ? init : sourceArray[0]
+    let i = (!!init) ? 0 : 1
+    for (; i < sourceArray.length; i++) {
+        agg = callback(sourceArray[i], agg)
+    }
+    return agg
+}
